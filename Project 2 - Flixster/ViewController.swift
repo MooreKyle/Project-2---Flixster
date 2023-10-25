405b9b724592d2cc6a1b9cb1d947600ea6e7ebbb
Kyle Moore <moorekyle2022@fau.edu>
2023-10-25 15:11:49 -0400

    Initial Commit

---
+++ Project 2 - Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2 - Flixster
+//
+//  Created by student on 10/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
