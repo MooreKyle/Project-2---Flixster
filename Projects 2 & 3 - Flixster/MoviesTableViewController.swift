f18fb66a6539116bc6de9d9cdaf19fc94b39a997
Kyle Moore <moorekyle2022@fau.edu>
2023-10-27 02:54:18 -0400

    - MoviesTableViewController.swift - Major Changes to Code - Fixed plist.info reference location needing updating in build settings from project name having been revised via xcode interface, where it referred to invalid, old location and old name of project: "Project 2 - Flixster".

--- Projects 2 & 3 - Flixster/MoviesTableViewController.swift
+++ Projects 2 & 3 - Flixster/MoviesTableViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class MoviesTableViewController: UITableViewController {
 
+    var movies: [Movie] = []
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        //Assigns the mock movie data to the movies property
+        movies = movieData
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,23 +29,28 @@ class MoviesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movies.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
 
         // Configure the cell...
+        let movie = movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+        cell.detailTextLabel?.text = movie.description
+     
+        //Set the image (Will need to load it from URL in part 2)
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
