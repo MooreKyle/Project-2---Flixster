1c06245135b58a79f6e1fd8f0265b371eae84fab
Kyle Moore <moorekyle2022@fau.edu>
2023-10-27 18:02:28 -0400

    Before ANY deletion of .xib files

---
+++ MovieDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MovieDetailViewController.swift
+//  Projects 2 & 3 - Flixster
+//
+//  Created by student on 10/27/23.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    
+    
+    //Displays Movie Details Selected by User - Popuklates UI Elements and Show the User Movie Information
+    var movie: Movie?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
