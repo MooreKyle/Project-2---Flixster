2efc206ff8f6903f6f3aff388ed71b0f67fac691
Kyle Moore <moorekyle2022@fau.edu>
2023-10-30 01:25:12 -0400

    MoviesViewController.swift translated to movies DetailViewController.swift translated to movies

--- lab-tunley/MoviesViewController.swift
+++ lab-tunley/MoviesViewController.swift
@@ -1,35 +1,35 @@
 //
-//  ViewController.swift
-//  lab-tunley
+//  MoviesViewController.swift
+//  Projects 2 & 3 - Flixster
 //
-//  Created by Charlie Hieger on 12/1/22.
+//  Created by Kyle Moore on 10/29/23.
 //
 
 import UIKit
 
-class TracksViewController: UIViewController, UITableViewDataSource {
+class MoviesViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tracks.count
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get a cell with identifier, "TrackCell"
         // the `dequeueReusableCell(withIdentifier:)` method just returns a generic UITableViewCell so it's necessary to cast it to our specific custom cell.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
 
-        // Get the track that corresponds to the table view row
-        let track = tracks[indexPath.row]
+        // Get the movie that corresponds to the table view row
+        let movie = movies[indexPath.row]
 
-        // Configure the cell with it's associated track
-        cell.configure(with: track)
+        // Configure the cell with it's associated movie
+        cell.configure(with: movie)
 
         // return the cell for display in the table view
         return cell
     }
     
 
-    // TODO: Pt 1 - Add a tracks property
-    var tracks: [Track] = []
+    // TODO: Pt 1 - Add a movies property
+    var movies: [Movie] = []
 
     // TODO: Pt 1 - Add table view outlet
     @IBOutlet weak var tableView: UITableView!
@@ -37,9 +37,9 @@ class TracksViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Pt 1 - Set tracks property with mock tracks array
-        tracks = Track.mockTracks
-        print(tracks)
+        // TODO: Pt 1 - Set movies property with mock movies array
+        movies = Movie.mockMovies
+        print(movies)
         
         tableView.dataSource = self
         
@@ -54,11 +54,11 @@ class TracksViewController: UIViewController, UITableViewDataSource {
            // Get the detail view controller
            let detailViewController = segue.destination as? DetailViewController {
 
-            // Use the index path to get the associated track
-            let track = tracks[indexPath.row]
+            // Use the index path to get the associated movie
+            let movie = movies[indexPath.row]
 
-            // Set the track on the detail view controller
-            detailViewController.track = track
+            // Set the movie on the detail view controller
+            detailViewController.movie = movie
         }
 
     }
