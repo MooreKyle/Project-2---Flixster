191ed3a790def88e4caef4b2ce7b93317f4c62f8
Kyle Moore <moorekyle2022@fau.edu>
2023-10-30 01:25:12 -0400

    MoviesViewController.swift translated to movies DetailViewController.swift translated to movies

--- lab-tunley/DetailViewController.swift
+++ lab-tunley/DetailViewController.swift
@@ -1,8 +1,8 @@
 //
 //  DetailViewController.swift
-//  lab-tunley
+//  Projects 2 & 3 - Flixster
 //
-//  Created by Charlie Hieger on 12/5/22.
+//  Created by Kyle Moore on 10/29/23.
 //
 
 import UIKit
@@ -10,40 +10,41 @@ import Nuke
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var trackImageView: UIImageView!
-    @IBOutlet weak var trackNameLabel: UILabel!
-
-    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieOverviewLabel: UILabel!
     @IBOutlet weak var albumLabel: UILabel!
-    @IBOutlet weak var genreLabel: UILabel!
-    @IBOutlet weak var releaseDateLabel: UILabel!
-    @IBOutlet weak var durationLabel: UILabel!
+    
+    //@IBOutlet weak var genreLabel: UILabel!
+    //@IBOutlet weak var releaseDateLabel: UILabel!
+    //@IBOutlet weak var durationLabel: UILabel!
 
-    // TODO: Pt 1 - Add a track property
+    // TODO: Pt 1 - Add a movie property
 
-    // A property to store the track object.
-    // We can set this property by passing along the track object associated with the track the user tapped in the table view.
-    var track: Track!
+    // A property to store the movie object.
+    // We can set this property by passing along the movie object associated with the movie the user tapped in the table view.
+    var movie: Movie!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Load the image located at the `artworkUrl100` URL and set it on the image view.
-        Nuke.loadImage(with: track.artworkUrl100, into: trackImageView)
+        // Load the image located at the `posterImageURL` URL and set it on the image view.
+        Nuke.loadImage(with: movie.posterImageURL, into: movieImageView)
 
-        // Set labels with the associated track values.
-        trackNameLabel.text = track.trackName
-        artistLabel.text = track.artistName
-        albumLabel.text = track.collectionName
-        genreLabel.text = track.primaryGenreName
+        // Set labels with the associated movie values.
+        movieTitleLabel.text = movie.title
+        movieOverviewLabel.text = movie.overview
+        
+        //albumLabel.text = track.collectionName
+        //genreLabel.text = track.primaryGenreName
 
         // Create a date formatter to style our date and convert it to a string
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        releaseDateLabel.text = dateFormatter.string(from: track.releaseDate)
+        //let dateFormatter = DateFormatter()
+        //dateFormatter.dateStyle = .medium
+        //movieReleaseDateLabel.text = dateFormatter.string(from: movie.releaseDate)
 
         // Use helper method to convert milliseconds into `mm:ss` string format
-        durationLabel.text = formattedTrackDuration(with: track.trackTimeMillis)
+        //durationLabel.text = formattedTrackDuration(with: track.trackTimeMillis)
 
     }
 
