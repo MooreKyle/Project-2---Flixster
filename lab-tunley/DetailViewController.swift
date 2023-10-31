49e46225269fcf74e9343ee56b905cc0d8f98130
Kyle Moore <moorekyle2022@fau.edu>
2023-10-31 14:30:52 -0400

    Using Lab 3 Completed Version as Template (No Changes Yet)      - Beginning of Project 3 - Flixster Part 2

--- lab-tunley/DetailViewController.swift
+++ lab-tunley/DetailViewController.swift
@@ -1,8 +1,8 @@
 //
 //  DetailViewController.swift
-//  Projects 2 & 3 - Flixster
+//  lab-tunley
 //
-//  Created by Kyle Moore on 10/29/23.
+//  Created by Charlie Hieger on 12/5/22.
 //
 
 import UIKit
@@ -10,46 +10,35 @@ import Nuke
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var movieImageView: UIImageView!
-    @IBOutlet weak var movieTitleLabel: UILabel!
-    
-    @IBOutlet weak var voteAverageLabel: UILabel!
-    
-    @IBOutlet weak var voteCountLabel: UILabel!
-    @IBOutlet weak var popularityLabel: UILabel!
-    //@IBOutlet weak var durationLabel: UILabel!
-    
-    @IBOutlet weak var movieOverviewLabel: UILabel!
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
 
-    // TODO: Pt 1 - Add a movie property
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
 
-    // A property to store the movie object.
-    // We can set this property by passing along the movie object associated with the movie the user tapped in the table view.
-    var movie: Movie!
+    var track: Track!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Load the image located at the `posterImageURL` URL and set it on the image view.
-        Nuke.loadImage(with: movie.posterImageURL, into: movieImageView)
-
-        // Set labels with the associated movie values.
-        movieTitleLabel.text = movie.title
-        movieOverviewLabel.text = movie.overview
-        
-        //albumLabel.text = track.collectionName
-        //genreLabel.text = track.primaryGenreName
+        Nuke.loadImage(with: track.artworkUrl100, into: trackImageView)
+        trackNameLabel.text = track.trackName
+        artistLabel.text = track.artistName
+        albumLabel.text = track.collectionName
+        genreLabel.text = track.primaryGenreName
 
         // Create a date formatter to style our date and convert it to a string
-        //let dateFormatter = DateFormatter()
-        //dateFormatter.dateStyle = .medium
-        //movieReleaseDateLabel.text = dateFormatter.string(from: movie.releaseDate)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        releaseDateLabel.text = dateFormatter.string(from: track.releaseDate)
 
         // Use helper method to convert milliseconds into `mm:ss` string format
-        //durationLabel.text = formattedTrackDuration(with: track.trackTimeMillis)
+        durationLabel.text = formattedTrackDuration(with: track.trackTimeMillis)
 
     }
-
 
 
 }
